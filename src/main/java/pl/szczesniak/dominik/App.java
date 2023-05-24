3712e118175b3c33a49d5c6637f13246e949e5a8
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-05-24 14:09:50 +0200

    tests and fibonacci

--- src/main/java/pl/szczesniak/dominik/App.java
+++ src/main/java/pl/szczesniak/dominik/App.java
@@ -3,31 +3,20 @@ package pl.szczesniak.dominik;
 public class App {
 
 
-	public static String SearchingChallenge(String str) {
-		String[] list = str.split(" ");
-		int highestCounted = 0;
-		String wordWithMostLetters = "";
-		for (String word : list) {
-			int letterCounter = 0;
-			for (int i = 0; i < word.length(); i++) {
-				char c = word.charAt(i);
-				for (int k = 0; k < word.length(); k++) {
-					if (k != i && word.charAt(k) == c) {
-						letterCounter++;
-					}
-				}
-				if (letterCounter > 1 && letterCounter > highestCounted) {
-					highestCounted = letterCounter;
-					wordWithMostLetters = word;
-				}
-			}
-			if (highestCounted > 1) {
-				String token = "9p0tmyw3e1";
-				String regex = "[" + token + "]";
-				return wordWithMostLetters.replaceAll(regex, "");
-			}
-			}
-		return "-1";
+	public static int ArrayChallenge(int[] arr) {
+		int result = 0;
+		for(int variable : arr) {
+			result += variable;
+		}
+
+		return arr[0];
+	}
+
+	public static int  fibonacci(int number) {
+		if (number < 2) {
+			return number;
+		}
+		return fibonacci(number-1) + fibonacci(number-2);
 	}
 
 
--- src/test/java/pl/szczesniak/dominik/AppTest.java
+++ src/test/java/pl/szczesniak/dominik/AppTest.java
@@ -3,14 +3,13 @@ package pl.szczesniak.dominik;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static pl.szczesniak.dominik.App.SearchingChallenge;
+import static pl.szczesniak.dominik.App.ArrayChallenge;
 
 class AppTest {
 
 	@Test
 	void name() {
-		assertThat(SearchingChallenge("mam na imie dominik")).isEqualTo("a");
-		assertThat(SearchingChallenge("No words")).isEqualTo("-1");
-		assertThat(SearchingChallenge("Hello apple pie")).isEqualTo("Hllo");
+		assertThat(ArrayChallenge(new int[] {5,2,1})).isEqualTo(2);
+		assertThat(ArrayChallenge(new int[] {1,20,2,5})).isEqualTo(6);
 	}
 }
