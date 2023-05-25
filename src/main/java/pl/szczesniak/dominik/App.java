1f83b89eb46b280028413ce2ea6a39c01068eef6
Dominik Szczęśniak <dominikszczesniak128@gmail.com>
2023-05-25 10:43:24 +0200

    moved everything ot main branch

--- src/main/java/pl/szczesniak/dominik/App.java
+++ src/main/java/pl/szczesniak/dominik/App.java
@@ -5,6 +5,60 @@ import java.util.List;
 
 public class App {
 
+	public static String SearchingChallenge(String str) {
+		String[] list = str.split(" ");
+		int highestCounted = 0;
+		String wordWithMostLetters = "";
+		for (String word : list) {
+			int letterCounter = 0;
+			for (int i = 0; i < word.length(); i++) {
+				char c = word.charAt(i);
+				for (int k = 0; k < word.length(); k++) {
+					if (k != i && word.charAt(k) == c) {
+						letterCounter++;
+					}
+				}
+				if (letterCounter > 1 && letterCounter > highestCounted) {
+					highestCounted = letterCounter;
+					wordWithMostLetters = word;
+				}
+			}
+			if (highestCounted > 1) {
+				String token = "9p0tmyw3e1";
+				String regex = "[" + token + "]";
+				return wordWithMostLetters.replaceAll(regex, "");
+			}
+		}
+		return "-1";
+	}
+
+
+	public static int ArrayChallenge(int[] arr) {
+		int result = 0;
+		for (int variable : arr) {
+			result += variable;
+		}
+
+		for (int i = 0; i < 1000; i++) {
+			if (fibonacci(result + i)) {
+				return i;
+			}
+		}
+
+		return arr[0];
+	}
+
+	public static boolean fibonacci(int number) {
+		int previousNumber = 0;
+		int currentNumber = 1;
+		while (currentNumber < number) {
+			int index = currentNumber;
+			currentNumber = previousNumber + currentNumber;
+			previousNumber = index;
+		}
+		return currentNumber == number;
+	}
+
 
 //	new String[] {"101",
 //				  "111",
--- src/test/java/pl/szczesniak/dominik/AppTest.java
+++ src/test/java/pl/szczesniak/dominik/AppTest.java
@@ -3,12 +3,28 @@ package pl.szczesniak.dominik;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static pl.szczesniak.dominik.App.ArrayChallenge;
 import static pl.szczesniak.dominik.App.MatrixChallenge;
+import static pl.szczesniak.dominik.App.SearchingChallenge;
 
 class AppTest {
 
+
 	@Test
-	void name() {
+	void searching() {
+		assertThat(SearchingChallenge("mam na imie dominik")).isEqualTo("a");
+		assertThat(SearchingChallenge("No words")).isEqualTo("-1");
+		assertThat(SearchingChallenge("Hello apple pie")).isEqualTo("Hllo");
+	}
+
+	@Test
+	void array() {
+		assertThat(ArrayChallenge(new int[] {5,2,1})).isEqualTo(0);
+		assertThat(ArrayChallenge(new int[] {1,20,2,5})).isEqualTo(6);
+	}
+
+	@Test
+	void matrix() {
 		assertThat(MatrixChallenge(new String[] {"1011", "0011", "0111", "1111"})).isEqualTo("8");
 		assertThat(MatrixChallenge(new String[] {"101", "111", "001"})).isEqualTo("3");
 	}
